31ba4bfeecc9356793fae88ba51e50d761abc7f3
Bram Reinold <bram_reinold@hotmail.com>
2023-10-17 10:43:17 +0200

    Nav with sidebar setup

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 
 import { Spinner } from "@/components/spinner";
+import Navigation from "./_components/navigation";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -22,7 +23,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
-      {/* <Navigation /> */}
+      <Navigation />
       Navigation
       <main className="flex-1 h-full overflow-y-auto">
         {/* <SearchCommand /> */}
